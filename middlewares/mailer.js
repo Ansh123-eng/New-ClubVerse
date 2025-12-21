3e506f1bf7a68ed6c26a40b0850cd0e090ac3033
Ansh123-eng <anshvohra22@gmail.com>
2025-12-21 19:42:10 +0530

    Membership fix

--- middlewares/mailer.js
+++ middlewares/mailer.js
@@ -1,11 +1,21 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  // Additional settings for Vercel/serverless environments
+  tls: {
+    rejectUnauthorized: false
+  },
+  // Connection timeout settings
+  connectionTimeout: 60000, // 60 seconds
+  greetingTimeout: 30000, // 30 seconds
+  socketTimeout: 60000 // 60 seconds
 });
 
 export default transporter;
